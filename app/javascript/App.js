67908dee6169948327008cea53d9212ae84852fa
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-11 09:44:52 +0530

    Routes path set-up

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './src/components/Home';
+import { Routes, Route } from 'react-router-dom';
 
 const  App = () => {
   return (
-    <>
-    <h1>Hello Hasina!</h1>
-    <h1>Hello James!</h1>
-    <h1>Hello Gilbert!</h1>
-    <h1>Welcome to Final capstone project. Greetings from Ralph</h1>
-    </>
+    <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
   );
 };
 
