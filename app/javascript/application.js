853d62bf053964ccf9e09c0e0c6df3d6f596fc8d
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-11 06:28:02 +0530

    React text

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const  App = () => {
+  return (
+    <>
+    <h1>Hello hassina!</h1>
+    <h1>Hello james!</h1>
+    <h1>Hello Gilbert!</h1>
+    <h1>Welcome to Final capstone project. greetings from Ralph</h1>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
