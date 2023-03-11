193e0e915871575835f3d8882189046a7240c3dc
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-11 07:26:34 +0530

    React componets

---
+++ app/javascript/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const  App = () => {
+  return (
+    <>
+    <h1>Hello Hasina!</h1>
+    <h1>Hello James!</h1>
+    <h1>Hello Gilbert!</h1>
+    <h1>Welcome to Final capstone project. Greetings from Ralph</h1>
+    </>
+  );
+};
+
+export default App;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,18 +5,9 @@ import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const  App = () => {
-  return (
-    <>
-    <h1>Hello hassina!</h1>
-    <h1>Hello james!</h1>
-    <h1>Hello Gilbert!</h1>
-    <h1>Welcome to Final capstone project. greetings from Ralph</h1>
-    </>
-  );
-}
+import App from "./App"
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+    <App/>,
+    document.getElementById('root'),
+  );
