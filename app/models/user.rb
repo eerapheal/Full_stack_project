a0a213a196ab92150ba0adcf45f88d78882f01af
masouma <masouma2020@gmail.com>
2023-03-13 12:47:25 +0430

    Add JWT and validation  in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+    include Devise::JWT::RevocationStrategies::JTIMatcher
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { in: 1..50 }, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
+  def jwt_payload
+    super
+  end
 end
