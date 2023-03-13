cb8e14088bd4e88177c5e9fea3fbaf9a8e2f34df
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-14 02:28:26 +0530

    Motocycle table validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :motorcycles
 
-  validates :user, :room, :start_date, :end_date, presence: true
+  validates :user, :motorcycles, :start_date, :end_date, presence: true
   validates :start_date, comparison: { greater_than_or_equal_to: Date.today, message: 'must be today or later' }
   validates :end_date, comparison: { greater_than: :start_date, message: 'must be after start date' }
 end
