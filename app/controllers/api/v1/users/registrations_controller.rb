4e0f34bda36f7eab3e3e4b4b732d0fe3132fe676
masouma <masouma2020@gmail.com>
2023-03-13 12:28:12 +0430

    MOdify registration controller and add json response

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,62 +1,26 @@
 # frozen_string_literal: true
 
 class  Api::V1::Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  respond_to :json
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+    resource.persisted? ? register_success(resource) : register_failed(resource)
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def register_success(resource)
+    render json: {
+      status: 200,
+      message: 'Signed up successfully.',
+      data: UserSerializer.new(resource)
+    }, status: :ok
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def register_failed(resource)
+    render json: {
+      status: 422,
+      message: "Signed up failure. #{resource.errors.full_messages.to_sentence}"
+    }, status: :unprocessable_entity
+  end
 end
