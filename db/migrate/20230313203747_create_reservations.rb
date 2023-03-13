e1001d7bcf01b3cebc97bb6f55593fe4f2d942bb
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-14 04:47:39 +0530

    Table specification

--- db/migrate/20230313203747_create_reservations.rb
+++ db/migrate/20230313203747_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :motorcycles, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: {to_table: :users}
+      t.references :motorcycles, null: false, foreign_key: {to_table: :motorcycles}
       t.date :start_date
       t.date :end_date
 
