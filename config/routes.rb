143f65eaa72f0ed943257c04910f36950610117f
masouma <masouma2020@gmail.com>
2023-03-13 11:24:49 +0430

    update route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'root#index'
-  devise_for :users, controllers: {
-     registrations: 'users/registrations',
-      sessions: 'users/sessions' 
-    }
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users, skip: [:sessions, :registrations],
+        controllers: {
+          sessions: 'api/v1/users/sessions',
+          registrations: 'api/v1/users/registrations',
+        }
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
