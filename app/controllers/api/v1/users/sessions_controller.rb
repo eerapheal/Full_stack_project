71b85c0ee18f67b52de93c5d72b59b929871503b
masouma <masouma2020@gmail.com>
2023-03-13 12:25:03 +0430

    Update session controller and add json responce

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
+  respond_to :json
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -9,19 +10,71 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_for_database_authentication(email: params[:user][:email])
+    if user && user.valid_password?(params[:user][:password])
+      sign_in(user)
+      render json: { message: 'Logged in successfully.', data: user }
+    else
+      render json: { message: 'Invalid email or password.' }, status: :unauthorized
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    if current_user
+      sign_out(current_user)
+      render json: { message: 'Logged out successfully.' }
+    else
+      render json: { message: 'You need to sign in or sign up before continuing.' }, status: :unprocessable_entity
+    end
+  end
 
-  # protected
+  private
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_with(_resource, _opts = {})
+  current_user ? log_in_success : log_in_failure
+  end
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
+                               ENV.fetch('DEVISE_JWT_SECRET_KEY')).first
+
+      current_user = User.find(jwt_payload['sub'])
+
+      current_user ? log_out_success : log_out_failure
+    else
+      log_out_failure
+    end
+  end
+
+  def log_in_success
+    render json: {
+      status: 200,
+      message: 'Logged in sucessfully.',
+      data: UserSerializer.new(current_user)
+    }, status: :ok
+  end
+
+  def log_in_failure
+    render json: {
+      status: 401,
+      message: "Logged in failure. #{resource.errors.full_messages.to_sentence}",
+      data: UserSerializer.new(current_user)
+    }, status: :unauthorized
+  end
+
+  def log_out_success
+    render json: {
+      status: 200,
+      message: 'Logged out sucessfully.'
+    }, status: :ok
+  end
+
+  def log_out_failure
+    render json: {
+      status: 401,
+      message: 'Logged out failure.'
+    }, status: :unauthorized
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
           registrations: 'api/v1/users/registrations',
         }
     end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
