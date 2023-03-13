6d954c2c0cc028228a2a368da997f75c4d23435b
masouma <masouma2020@gmail.com>
2023-03-13 11:16:06 +0430

    generates customized controllers for the Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'root#index'
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+     registrations: 'users/registrations',
+      sessions: 'users/sessions' 
+    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
