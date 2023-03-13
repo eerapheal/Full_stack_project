6dfc5dfbe9c6162cd0f05fd5b8f03ffaebfb6324
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-14 02:52:52 +0530

    Tables migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_082333) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_203747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "motorcycles", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.text "description"
+    t.decimal "price"
+    t.string "motorcycle_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "motorcycles_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["motorcycles_id"], name: "index_reservations_on_motorcycles_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -29,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_082333) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "motorcycles", column: "motorcycles_id"
+  add_foreign_key "reservations", "users"
 end
