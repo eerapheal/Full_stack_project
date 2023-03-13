3f078450728a94fa8f9387c9ed517275ef23c749
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-14 02:33:14 +0530

    User asociation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { in: 1..50 }, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+
+  has_many :reservations, dependent: :destroy
+  has_many :motorcycles, through: :reservations, dependent: :destroy
+
 end
