86f942a3ef3f7c9258b4f098532345cb33073b8f
masouma <masouma2020@gmail.com>
2023-03-13 11:01:20 +0430

    specify the controller for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'root#index'
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
