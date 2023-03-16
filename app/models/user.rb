e67b2e28c683c21ee2952036ad40924f0a6a14fb
Gilbert <okonjogilbert2@gmail.com>
2023-03-16 11:49:09 +0300

    Add motorcycle controller for API

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
-  include Devise::JWT::RevocationStrategies::JTIMatcher
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { in: 1..50 }, uniqueness: true
-  validates :password, presence: true, length: { in: 6..20 }
 
   has_many :reservations, dependent: :destroy
   has_many :motorcycles, through: :reservations, dependent: :destroy
