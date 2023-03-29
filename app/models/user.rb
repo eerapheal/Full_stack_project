aa7d58f2f5e3ed60dbb52023860060716e65a6a3
jaamanu <jaamanu@gmail.com>
2023-03-29 23:32:09 +0200

    fix errors on model user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
 
   has_many :reservations, dependent: :destroy
   has_many :motorcycles, through: :reservations, dependent: :destroy
